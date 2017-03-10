7f6ae8807a818df4688ceeda67128185d006a21c
Devon Kinghorn <devonkinghorn@Scotts-MBP.app.byu.edu>
2017-03-10 09:06:53 -0700

    rock paper scissors

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-app.use(express.static('public'));
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
+let userHandlers = require('./lib/handlers/userHandlers')
+let gameHandlers = require('./lib/handlers/gameHandlers')
+MongoClient = require('mongodb').MongoClient
+var MONGO_DB_URL = process.env.MONGO_DB || 'mongodb://localhost:27017/rockpaperscissors';
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+
+MongoClient.connect(MONGO_DB_URL, function (err, db) {
+  if (err) {
+    console.error('failed to connect to mongo')
+    return;
+  }
+  app.use(express.static('public'));
+
+  io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+      io.emit('chat message', msg);
+    });
+    socket.on('invite', gameHandlers.invite(io, socket, db))
+    socket.on('register', userHandlers.register(io, socket, db))
+    socket.on('acceptPlay', gameHandlers.acceptPlay(io, socket, db))
+    socket.on('throw', gameHandlers.acceptThrow(io, socket, db))
+  });
+
+  http.listen(8080, function () {
+    console.log('listening on *:8080');
   });
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
-});
---
+++ public/index.js
@@ -0,0 +1,99 @@
+var app = angular.module('myApp', []);
+    app.controller('myCtrl',
+      function ($scope, $window, $http) {
+        var socket = io();
+        $scope.opponent = '';
+        $scope.login = true;
+        $scope.search = true;
+        $scope.searchText = ''
+        $scope.check = function (username) {
+          console.log(username)
+          if (username != '') {
+            console.log('signing in')
+            socket.emit('register', {
+              userName: username
+            })
+          }
+        };
+        $scope.play = function (invited) {
+          if (invited.button == 'invite') {
+            socket.emit('invite', {
+              inviter: $scope.username,
+              invited: invited
+            })
+          } else {
+            socket.emit('acceptPlay', {
+              player1: {name: $scope.username},
+              player2: invited
+            })
+          }
+        }
+        $scope.sendMessage = function (e) {
+          e.preventDefault();
+          console.log(e);
+          if (!$scope.messageInput) return;
+          socket.emit('invite', {
+            text: $scope.messageInput,
+            user: $scope.user,
+            //   avatar: $scope.avatar
+          });
+          $scope.messageInput = '';
+        }
+        $scope.back = () => {
+          $scope.search = true;
+        }
+        $scope.throw = (_throw) => {
+          socket.emit('throw',{throw: _throw, game: $scope.game})
+        }
+        var invitations = {};
+        socket.on('updateplayers', function (msg) {
+          $scope.$apply(function () {
+            $scope.currentUsers = msg.map(function (player) {
+              return {
+                name: player.name,
+                socket: player.socket,
+                button: (invitations[player.name] ? 'play' : 'invite')
+              }
+            }).filter(user => user.name != $scope.username);
+          })
+        });
+        socket.on('login', function (msg) {
+          $scope.$apply(function () {
+            if (msg.error) {
+              alert(msg.message)
+            } else {
+              $scope.username = msg.userName;
+              $scope.login = false;
+              $scope.search = true;
+            }
+          })
+        })
+        socket.on('startGame', function (msg) {
+          $scope.$apply(function () {
+            if ($scope.search) {
+              $scope.search = false;
+              $scope.game = msg
+            }
+          })
+        })
+        socket.on('nextRound', function(msg){
+          $scope.$apply(function () {
+            debugger;
+            console.log(msg)
+          })
+        })
+        socket.on('invite', function (msg) {
+          
+          $scope.$apply(function () {
+            if (msg.invited.name == $scope.username) {
+              invitations[msg.inviter.name] = true;
+              $scope.currentUsers.forEach(function (user) {
+                if (invitations[msg.inviter.name]) {
+                  user.button = 'play';
+                }
+              })
+              
+            }
+          })
+        });
+      })
